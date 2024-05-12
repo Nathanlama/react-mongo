8ed945230267ceef0e17f5573ecf348906c6b5ae
Nathanlama <nurbuhyolmo10@gmail.com>
2024-05-12 21:19:49 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+function Bank(name) {
+  let _balance = 0;
+  this.name = name;
+
+  this.getBalance = function () {
+    return _balance;
+  }
+
+  this.setBalance = function (amount) {
+    _balance += amount;
+  }
+
 }
 
-export default App;
+const App = () => {
+
+  const per = new Bank('prabhu');
+  per.setBalance(90000);
+
+
+  console.log(per.getBalance());
+
+
+
+  return (
+    <div>
+
+
+    </div>
+  )
+}
+export default App
