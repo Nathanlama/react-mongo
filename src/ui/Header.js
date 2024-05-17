85e3c06a4e8d6e20e3740de92238717fcec543b8
Nathanlama <nurbuhyolmo10@gmail.com>
2024-05-17 08:26:21 +0545

    lme

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
 
 
 const Header = () => {
@@ -7,15 +6,6 @@ const Header = () => {
     <div className='bg-gray-900 text-white flex justify-between items-baseline px-4 py-2'>
 
       <h1 className='text-2xl '>TailWind</h1>
-
-      <nav className='space-x-4'>
-        <NavLink to='/about' className={(e) => {
-          console.log(e);
-        }}>About</NavLink>
-        <NavLink to='/contact'>Contact</NavLink>
-
-      </nav>
-
 
 
     </div>
---
+++ src/ui/NotFound.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useNavigate } from 'react-router'
+
+const NotFound = () => {
+
+  const nav = useNavigate();
+
+
+  return (
+    <div>
+      <h1>Not Found oke</h1>
+      <button onClick={() => nav(-1)}>Go Back</button>
+    </div>
+  )
+}
+
+export default NotFound
