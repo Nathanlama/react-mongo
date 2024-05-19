5285a33301409020093d39afbfae4d0889e1144d
Nathanlama <nurbuhyolmo10@gmail.com>
2024-05-19 22:54:10 +0545

    change

--- src/features/Home.js
+++ src/features/Home.js
@@ -1,15 +1,26 @@
 import React from 'react'
-import { Outlet } from 'react-router'
-import { NavLink } from 'react-router-dom'
+import { products } from '../dummy/Data';
 
+
+const personName = 'lios';
 const Home = () => {
   return (
-    <div>
-      <h1>This is Home</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab nulla neque architecto vitae, animi voluptates hic sunt qui iure ullam voluptas placeat omnis eos, aut atque voluptatum tempore temporibus officia vel quae cumque quos. Facere atque accusantium accusamus possimus repellat ipsum sunt alias ipsam nam officia, modi nemo esse voluptates.</p>
-      <NavLink to='/' className='bg-orange-500'>Go to Child1</NavLink>
-      <NavLink to='/child2' className='bg-cyan-400'>Go to Child2</NavLink>
-      <Outlet />
+    <div className='p-4'>
+
+      <div>
+        {products.map((product) => {
+          return <div key={product.id} className='space-y-2 cursor-pointer'>
+            <h1 className='text-2xl'>{product.title}</h1>
+            <img src={product.thumbnail} alt="" />
+
+          </div>
+        })}
+
+      </div>
+
+
+
+
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js}"
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-
-      gridTemplateColumns: {
-        autofit: 'repeat(auto-fit, minmax(300px, 1fr))'
-      }
-
-    },
-
-    screens: {
-      '2xl': { 'max': '1536px' },
-      'xl': { 'max': '1280px' },
-      'lg': { 'max': '1024px' },
-      'md': { 'max': '768px' },
-      'sm': { 'max': '640px' },
-    }
-
-
+    extend: {},
   },
   plugins: [],
 }
-
